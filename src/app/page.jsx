c4ee558d9dae6098277cfbc2441d75cfa5553ee3
sickmyducks <82773419+sickmyducks@users.noreply.github.com>
2024-06-10 20:52:37 +0700

    edit layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import Header from "./components/Header";
 import Pokedata from "./components/Pokedata";
 
 export default function Home() {
   return (
       <>
-      <Header />
       <Pokedata />
       </>
   );
